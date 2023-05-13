49fd79f9c646912086306f3b7c3242ab30769359
Rafael Nicolau <0xrafaelnicolau@gmail.com>
2023-05-13 17:14:36 +0100

    feat: add tokenuri & hacker XP update.
    The contract can now update the level of each hacker, and update
    the image of the NFT accordingly.

--- backend/src/HackerPass.sol
+++ backend/src/HackerPass.sol
@@ -4,12 +4,18 @@ pragma solidity ^0.8.13;
 import {ByteHasher} from "./helpers/ByteHasher.sol";
 import {IWorldID} from "./interfaces/IWorldID.sol";
 import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {Counters} from "../lib/openzeppelin-contracts/contracts/utils/Counters.sol";
+import {ERC721URIStorage} from "../lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract HackerPass is ERC721 {
+contract HackerPass is ERC721, ERC721URIStorage {
     using ByteHasher for bytes;
+    using Counters for Counters.Counter;
 
     error InvalidNullifier();
     error InvalidXpAmount();
+    error CannotTransferHackerPass();
+    error MaxLevelReached();
+    error DoesNotHaveHackerPass();
 
     // Worldcoin related.
     IWorldID internal immutable worldId;
@@ -17,23 +23,41 @@ contract HackerPass is ERC721 {
     uint256 internal immutable groupId = 1;
     mapping(uint256 => bool) internal nullifierHashes;
 
+    // Constants
+    uint256 private constant MAX_LEVEL = 10;
+    uint256 private constant MUL_FACTOR = 250;
+
     // Collection related.
-    uint256 public supply;
+    Counters.Counter private _tokenIdCounter;
     mapping(address => uint256) public xpOfHacker;
-    mapping(address => uint8) public levelOfHacker;
+    mapping(address => uint256) public levelOfHacker;
+    mapping(address => uint256) public tokenIdOfHacker;
+    string[] IpfsUri = [
+        "ipfs://QmabkMt2mKsnWwTgJCQtEbkjTobENpQZFEwvTQhwQ78hKk/HackerPass0.json",    
+        "ipfs://QmabkMt2mKsnWwTgJCQtEbkjTobENpQZFEwvTQhwQ78hKk/HackerPass1.json",
+        "ipfs://QmabkMt2mKsnWwTgJCQtEbkjTobENpQZFEwvTQhwQ78hKk/HackerPass2.json",
+        "ipfs://QmabkMt2mKsnWwTgJCQtEbkjTobENpQZFEwvTQhwQ78hKk/HackerPass3.json",
+        "ipfs://QmabkMt2mKsnWwTgJCQtEbkjTobENpQZFEwvTQhwQ78hKk/HackerPass4.json",
+        "ipfs://QmabkMt2mKsnWwTgJCQtEbkjTobENpQZFEwvTQhwQ78hKk/HackerPass5.json",
+        "ipfs://QmabkMt2mKsnWwTgJCQtEbkjTobENpQZFEwvTQhwQ78hKk/HackerPass6.json",
+        "ipfs://QmabkMt2mKsnWwTgJCQtEbkjTobENpQZFEwvTQhwQ78hKk/HackerPass7.json",
+        "ipfs://QmabkMt2mKsnWwTgJCQtEbkjTobENpQZFEwvTQhwQ78hKk/HackerPass8.json",
+        "ipfs://QmabkMt2mKsnWwTgJCQtEbkjTobENpQZFEwvTQhwQ78hKk/HackerPass9.json",
+        "ipfs://QmabkMt2mKsnWwTgJCQtEbkjTobENpQZFEwvTQhwQ78hKk/HackerPass10.json"
+    ];
 
     constructor(
         IWorldID _worldId,
         string memory _appId,
         string memory _actionId
-    ) ERC721("Hacker Pass", "HP") 
-    {
+    ) ERC721("Hacker Pass", "HP") {
         worldId = _worldId;
         externalNullifier = abi
             .encodePacked(abi.encodePacked(_appId).hashToField(), _actionId)
             .hashToField();
     }
 
+    // this function works as verification and mint function 
     function verifyAndMint(address signal, 
         uint256 root, 
         uint256 nullifierHash, 
@@ -52,26 +76,78 @@ contract HackerPass is ERC721 {
 
         nullifierHashes[nullifierHash] = true;
 
-        supply += 1;
-
-        _mint(signal, supply);
+        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
+        tokenIdOfHacker[signal] = tokenId;
+        _safeMint(signal, tokenId);
+        _setTokenURI(tokenId, IpfsUri[0]);
     }
     
-    // TODO: test these function.
-    function updateHackerXp(address _hacker, uint256 _hackerXp) external {
-        // if (Contest.!deployedContests[msg.sender]) revert InvalidContest();
+    function updateHackerXp(address _hacker, uint256 _earnedXp) public {
+        // TODO: if (Contest.!deployedContests[msg.sender]) revert InvalidContest();
         
-        if (_hackerXp != 0) revert InvalidXpAmount();
+        if (_earnedXp == 0) revert InvalidXpAmount();
 
-        xpOfHacker[_hacker] += _hackerXp;
+        xpOfHacker[_hacker] += _earnedXp;
 
-        updateHackerLevel(_hacker, _hackerXp);
+        updateHackerLevel(_hacker);
     }
 
-    // TODO: test these function.
-    function updateHackerLevel(address _hacker, uint256 _hackerXp) private {
-        // TODO: math function to determine if the hacker has leveled up
-        // if the hacker has leveled up increment it's level.
-        // else does nothing.
+    function updateHackerLevel(address _hacker) private {
+        uint256 requiredXp;
+        uint256 hackerLevel = levelOfHacker[_hacker];
+        uint256 currentXp = xpOfHacker[_hacker];
+        uint256 newLevel = hackerLevel;
+
+        for (uint i = 1; i <= hackerLevel; i++) {
+            requiredXp += calculateLevelXp(i);
+        }
+
+        for (uint i = newLevel + 1; i <= MAX_LEVEL; i++) {
+            uint256 xpForNextLevel = calculateLevelXp(i);
+            if (currentXp < xpForNextLevel) {
+                break;
+            }
+            newLevel = i;
+            currentXp -= xpForNextLevel;
+        }
+
+        uint256 tokenId = tokenIdOfHacker[_hacker];
+        if(tokenId == 0) revert DoesNotHaveHackerPass(); // FALHA ESTE CHECK
+
+        levelOfHacker[_hacker] = newLevel;
+        string memory newUri = IpfsUri[newLevel];
+
+        _setTokenURI(tokenId, newUri);
     }
+
+    function calculateLevelXp(uint256 _level) private pure returns (uint256) {
+        return 1000 + (_level - 1) * MUL_FACTOR;
+    } 
+
+    function _beforeTokenTransfer(
+        address from, 
+        address to, 
+        uint256 tokenId
+    ) internal virtual {
+        if (from != address(0)) revert CannotTransferHackerPass();
+        super._beforeTokenTransfer(from, to, tokenId, 1);  
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }   
 }
