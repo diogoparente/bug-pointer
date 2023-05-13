ac14e83650b6ea5ccceb8a72c16692ef5d9d1e7f
rodrigof1307 <rodrigo.fernandes.1307@gmail.com>
2023-05-13 17:32:04 +0100

    feat: create add-finding interface

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 import { ConnectButton } from "../connect-button";
 import { FadingLine } from "../fading-line";
+
 function Navbar() {
   return (
     <nav className="relative flex w-full items-center justify-between px-10 py-6">
-      <div className="h-8 w-8 rounded-full bg-gradient-to-r from-mainPurple to-mainGreen" />
+      <Link href="/">
+        <div className="h-8 w-8 rounded-full bg-gradient-to-r from-mainPurple to-mainGreen" />
+      </Link>
       <ConnectButton />
       <FadingLine className="absolute bottom-0 left-0" />
     </nav>
