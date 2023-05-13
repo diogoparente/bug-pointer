d76b1cd56f30ec723a99ca165f603baad31481e6
diogoparente <diogoparente.mypersonal@gmail.com>
2023-05-13 18:59:34 +0100

    feat: add logo

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import { ConnectButton } from "../connect-button";
 import { FadingLine } from "../fading-line";
+import Image from "next/image";
 
 function Navbar() {
   return (
-    <nav className="relative flex w-full items-center justify-between px-10 py-6">
+    <nav className="relative flex w-full items-center justify-between px-5 py-2">
       <Link href="/">
-        <div className="h-8 w-8 rounded-full bg-gradient-to-r from-mainPurple to-mainGreen" />
+        <Image alt="bug-pointer logo" src={"/logo.png"} height={75} width={75} />
       </Link>
       <ConnectButton />
       <FadingLine className="absolute bottom-0 left-0" />
