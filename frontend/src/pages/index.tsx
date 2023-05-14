818c6295b018684c353e8e5a79efb272e07b39e6
diogoparente <diogoparente.mypersonal@gmail.com>
2023-05-14 04:49:17 +0100

    fix: nft card

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -8,6 +8,8 @@ export default function Home() {
     <Page>
       <main className="flex flex-1 flex-col items-center justify-center gap-20">
         <Header className="px-32 text-center text-white">A sybil-resistant contest platform</Header>
+
+        <NftCard />
         <div className="flex flex-row items-center justify-center gap-16">
           <CustomLink type="button" color="purple" className="text-xl" size="large" href={"/create-contest"}>
             I&#39;m a Sponsor
@@ -15,8 +17,6 @@ export default function Home() {
           <CustomLink type="button" color="green" className="text-xl" size="large" href={"/hacker-onboarding"}>
             I&#39;m a Hacker
           </CustomLink>
-
-          <NftCard />
         </div>
       </main>
     </Page>
